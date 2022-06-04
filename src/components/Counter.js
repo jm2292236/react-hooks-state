c7bb6a845d9681b327e8a3e50afa5cab5e4e0d65
Jaime Montes de Oca <jaime0422@live.com>
2022-06-03 22:01:03 -0700

    Completed Assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  function increment() {
-    setCount(count + 1);
-  }
+    function doIncrement(count) {
+        // Setting state is asynchronous
+        // If we want to have access to the actual state value we need to use a callback function
+        count = count + 1;
+        console.log(count);
+        return count;
+    }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+    function increment() {
+        // Setting state is asynchronous
+        // If we want to have access to the actual state value we need to use a callback function
+        setCount((count) => {count = count + 1; console.log(`count = ${count}`); return count});
+
+        setCount((count) => doIncrement(count));
+    }
+
+    return (
+        <div>
+            <button onClick={increment}>I have been clicked {count} times</button>
+        </div>
+    )
 }
 
 export default Counter;
